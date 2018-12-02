d24a1a1743bb85476094355dbfe24e7683bc7742
feluso <feluso2@gmail.com>
2018-12-02 18:31:54 -0500

    Day 1 part 2

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -6,14 +6,33 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(new File("input"));
         int result = 0;
-        while (scanner.hasNext()) {
+        boolean[] alreadyVisited = new boolean[200000];
+        boolean[] alreadyVisitedNegative = new boolean[200000];
+        alreadyVisited[result] = true;
+        Scanner scanner = new Scanner(new File("input"));
+        boolean found = false;
+        while (!found) {
             String s = scanner.next();
             String op = s.substring(0, 1);
             int num = Integer.valueOf(s.substring(1));
             result = op.equals("-") ? result - num : result + num;
-            System.out.println(result);
+            boolean negative = result < 0;
+            int absResult = Math.abs(result);
+            if ((negative && alreadyVisitedNegative[absResult]) ||
+                    (!negative && alreadyVisited[result])) {
+                System.out.println(result);
+                found = true;
+                continue;
+            }
+            if (negative) {
+                alreadyVisitedNegative[absResult] = true;
+            } else {
+                alreadyVisited[result] = true;
+            }
+            if (!scanner.hasNext()) {
+                scanner = new Scanner(new File("input"));
+            }
         }
     }
 }
